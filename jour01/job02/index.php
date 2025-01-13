cacd2a34b549a08a4c56d76125e8e5402ee1044a
ennys-hammoud <ennys.hammoud@laplateforme.io>
2025-01-13 20:51:42 +0100

    Ajout de .gitignore pour ignorer les fichiers système

---
+++ jour01/job02/index.php
@@ -0,0 +1,9 @@
+<?php
+// Cette ligne affiche "Hello LaPlateforme!" une première fois
+echo "Hello LaPlateforme!<br>";
+
+/*
+Cette ligne affiche "Hello LaPlateforme!" une seconde fois
+*/
+echo "Hello LaPlateforme!<br>";
+?>
