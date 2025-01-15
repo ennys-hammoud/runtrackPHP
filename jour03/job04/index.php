7871895125d8a834b019a420303baa4dd1c212c0
ennys-hammoud <ennys.hammoud@laplateforme.io>
2025-01-15 11:48:12 +0100

    fin du job04

---
+++ jour03/job04/index.php
@@ -0,0 +1,10 @@
+<?php
+// Déclaration de la chaîne
+$str = "Dans l'espace, personne ne vous entend crier";
+
+// Comptage du nombre de caractères
+$nombre_de_caracteres = strlen($str);
+
+// Affichage du résultat
+echo "Le nombre de caractères dans la chaîne est : " . $nombre_de_caracteres;
+?>
