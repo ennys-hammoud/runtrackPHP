b3cb113f7e3b77e3098cda822fa0369e4a30f770
ennys-hammoud <ennys.hammoud@laplateforme.io>
2025-01-15 13:32:55 +0100

    fin du job06

---
+++ jour03/job06/index.php
@@ -0,0 +1,10 @@
+<?php
+// Définir la chaîne à inverser
+$str = "Les choses que l'on Possède finissent par nous posséder.";
+
+// Inverser la chaîne
+$reversed_str = strrev($str);
+
+// Afficher la chaîne inversée
+echo $reversed_str;
+?>
