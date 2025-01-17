6c8cfd987f84df0838e80d155c28281f2a083d7e
ennys-hammoud <ennys.hammoud@laplateforme.io>
2025-01-17 18:35:20 +0100

    fin job 03

---
+++  jour05/job03/index.php
@@ -0,0 +1,12 @@
+<?php
+// Définition de la fonction getHello
+function getHello() {
+    return "Hello LaPlateforme!";
+}
+
+// Appel de la fonction et récupération de la valeur retournée
+$message = getHello();
+
+// Affichage de la valeur retournée
+echo $message;
+?>
