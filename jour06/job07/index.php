c74b92860776a90d496ba81947bf3cbe1f44e3d7
ennys-hammoud <ennys.hammoud@laplateforme.io>
2025-01-20 11:36:48 +0100

    modification job 07

--- jour06/job07/index.php
+++ jour06/job07/index.php
@@ -4,14 +4,12 @@ function bubblesort($tab, $croissant) {
     for ($i = 0; $i < $n; $i++) {
         for ($j = 0; $j < $n - $i - 1; $j++) {
             if ($croissant) {
-                // Tri croissant
                 if ($tab[$j] > $tab[$j + 1]) {
                     $temp = $tab[$j];
                     $tab[$j] = $tab[$j + 1];
                     $tab[$j + 1] = $temp;
                 }
             } else {
-                // Tri décroissant
                 if ($tab[$j] < $tab[$j + 1]) {
                     $temp = $tab[$j];
                     $tab[$j] = $tab[$j + 1];
@@ -23,16 +21,19 @@ function bubblesort($tab, $croissant) {
     return $tab;
 }
 
-// Exemple d'utilisationr
 $tab = ["abc", "ghi", "def"];
 
 // Tri croissant
 $result_croissant = bubblesort($tab, true);
-echo "Tri croissant : ";
+echo "<h3>Tri croissant :</h3>";
+echo "<pre>";
 print_r($result_croissant);
+echo "</pre>";
 
 // Tri décroissant
 $result_decroissant = bubblesort($tab, false);
-echo "Tri décroissant : ";
+echo "<h3>Tri décroissant :</h3>";
+echo "<pre>";
 print_r($result_decroissant);
+echo "</pre>";
 ?>
