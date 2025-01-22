5f18a715a6b129c9d0554fffa3e6d3a24ea14c37
ennys-hammoud <ennys.hammoud@laplateforme.io>
2025-01-22 15:36:08 +0100

    fin job 09

---
+++ jour08/job9.sql
@@ -0,0 +1,4 @@
+SELECT * 
+FROM etudiants 
+WHERE YEAR(CURDATE()) - YEAR(naissance) < 18 
+  OR (YEAR(CURDATE()) - YEAR(naissance) = 18 AND DATE_FORMAT(naissance, '%m-%d') > DATE_FORMAT(CURDATE(), '%m-%d'));
